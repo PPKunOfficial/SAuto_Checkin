0cb4b3dce4483d9754fa13e1cdce613c5aaa861a
PP Kun <40565728+PPKunOffical@users.noreply.github.com>
2020-08-24 11:45:43 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -16,7 +16,10 @@ for i in accouts["sp"]:
 			}
 	print("User-Agent:",headers)
 	lose=requests.Session()
-	login_res=lose.post(login_url,data=login_data,headers=headers).content.decode("unicode-escape")
-	checkin_res=lose.post(checkin_url,headers=headers).content.decode("unicode-escape")
-	data_checkin=checkin_res
-	print("Return Json:",data_checkin,"\n")
+	try:
+		login_res=lose.post(login_url,data=login_data,headers=headers).content.decode("unicode-escape")
+		checkin_res=lose.post(checkin_url,headers=headers).content.decode("unicode-escape")
+		data_checkin=checkin_res
+		print("Return Json:",data_checkin,"\n")
+	except:
+		print("Error!")
